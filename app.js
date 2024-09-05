f35c2a1b78392644e93a1ed880a4559f7b622c40
DustinAguanta <dustinagustin011@gmail.com>
2024-09-05 20:01:08 +0300

    Initial commit with app.js

---
+++ app.js
@@ -0,0 +1,72 @@
+// Data types
+//String
+
+let name = "Dustin";
+console.log ("Yourname: "+ name);
+let age = 19;
+console.log ("Your age: "+ age);
+
+
+
+// Arithmetic and Logical Operators 
+
+let a = 100;
+let b = 19;
+let diff = a -b;
+console.log ("My grades: " + diff);
+
+//Conditional Statement
+
+if(age>=50){
+    console.log("gwapo ko");
+}else;
+console.log ("Pangit ka");
+
+let grado = 80;
+let category;
+
+switch(true){
+    case (grado>=75):
+        category = "Pass";
+break;
+default:
+    category = "Failed";
+    break;
+
+    console.log("Kaya pa");
+
+    //loops 
+
+const activities = ["EAT,PLAY,SLEEP,REAPET"];
+for (let w = 0; w < activities.length w++){
+    console.assert;ongamepadconnected( 'activities ${w + 1}: $ {activities [w]');
+
+}
+
+}
+
+//function Programming
+// Determine odd or even
+
+function CheckNumber(Number){
+    const numberToCheck = 30;
+    if (number % 2 === 0){
+        console.log('$ {number} is even.');
+    }else if (isPrime(number)) {
+        console.log('$ {number} is Prime.');
+    }else{
+        console.log("the number is prime");
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
